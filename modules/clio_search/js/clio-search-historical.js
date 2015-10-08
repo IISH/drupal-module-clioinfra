456a7a34f9177d25b0e2f29a2d3bb815841c64a7
eindgebruiker <gaele@guruburu.com>
2015-10-08 17:42:20 +0200

    Let historical js accept country id's in format datasetname/1234. Use session to store selection.

--- modules/clio_search/js/clio-search-historical.js
+++ modules/clio_search/js/clio-search-historical.js
@@ -14,6 +14,7 @@
           });
 
           // Add typeahead to search box.
+          var day = '-' + Drupal.settings.clioSearch.month + '-' + Drupal.settings.clioSearch.day;
           $('#geobox').typeahead({
             dynamic: true,
             // delay: 300,
@@ -22,8 +23,8 @@
             offset: true, // Country name should start with input string.
             accent: true, // Ignore accents.
             template: '<li class="ms-elem-selectable" id="{{id}}-selectable">' + 
-              '<span>{{name}}</span></li>',
-            display: "name",
+              '<span>{{year}}</span></li>',
+            display: "year", // This is the country name with begin and end year in brackets.
             source: {
               url: {
                 type: "GET",
@@ -38,11 +39,14 @@
             },
             callback: {
               onClick: function (node, a, item, event) {
+                // Item id's are like "geocron/12345"; we only need the numeric part.
+                var parts = item.id.split('/');
+                var id = parts[1];
                 // Add item and move to selected box.
                 $('#edit-country')
-                  .multiSelect('addOption', {value: item.id, text: item.name});
+                  .multiSelect('addOption', {value: id, text: item.year});
                 $('#edit-country')
-                  .multiSelect('select', item.id.toString());
+                  .multiSelect('select', id.toString());
                 // Do not show selected item in input field.
                 node[0].val("");
               },
